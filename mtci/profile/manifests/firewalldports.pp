35886683d31ed5d5124fe808cec2504589c3d3d4
mbijeff <mbijeff@gmail.com>
2017-12-18 14:21:13 -0500

    added firewalld2

---
+++ mtci/profile/manifests/firewalldports.pp
@@ -0,0 +1,31 @@
+class profile::firewalldports (){
+
+include firewalld
+
+firewalld_service { 'Allow SSH from the external zone':
+  ensure  => 'present',
+  service => 'ssh',
+  zone    => 'public',
+}
+
+firewalld_service { 'Allow ldaps from the public zone':
+  ensure  => 'present',
+  service => 'ldaps',
+  zone    => 'public',
+}
+
+
+firewalld_service { 'Allow dhcpv6 from the external zone':
+  ensure  => 'present',
+  service => 'dhcpv6-client',
+  zone    => 'public',
+}
+
+firewalld::custom_service{'talend_port_range':
+  short       => 'talend_fw_port',
+  description => 'Firewalld talend port range',
+port => [ {'port' => '8000:8002', 'protocol' => 'tcp']} ]
+
+}
+
+}
