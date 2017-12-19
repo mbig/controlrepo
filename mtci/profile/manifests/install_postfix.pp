8e44b79a4d583f97d1c790f934f7feb9e7099495
mbijeff <mbijeff@gmail.com>
2017-12-19 16:35:28 -0500

    added tls

--- mtci/profile/manifests/install_postfix.pp
+++ mtci/profile/manifests/install_postfix.pp
@@ -29,5 +29,12 @@ class profile::install_postfix() {
      }
 
 
+    postfix::config {
+  'smtp_tls_mandatory_ciphers':       value   => 'high';
+  'smtp_tls_security_level':          value   => 'secure';
+  'smtp_tls_CAfile':                  value   => '/etc/pki/tls/certs/ca-bundle.crt';
+  'smtp_tls_session_cache_database':  value   => 'btree:${data_directory}/smtp_tls_session_cache';
+}
+
 }
  
