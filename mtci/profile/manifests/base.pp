093ca051acad8dc10359f4a49b9a7e9465608c05
mbijeff <mbijeff@gmail.com>
2017-12-19 14:22:42 -0500

    added install_postfix

--- mtci/profile/manifests/base.pp
+++ mtci/profile/manifests/base.pp
@@ -4,30 +4,5 @@ class profile::base {
  include profile::firewalldports
  include graylog::allinone
  include java
-
-     class { 'postfix':
-
-  postfix::config { 'relay_domains':
-       ensure => present,
-       value  => 'localhost int.cybraics.com'
-     }
-
-   postfix::config { 'relayhost':
-       ensure => present,
-       value  => 'service-smtp.int.cybraics.com'
-     }
-     postfix::config { 'smtp_use_tls':
-       ensure => present,
-       value  => 'yes'
-     }
-
-postfix::config {
-  'smtp_tls_mandatory_ciphers':       value   => 'high';
-  'smtp_tls_security_level':          value   => 'secure';
-  'smtp_tls_CAfile':                  value   => '/etc/pki/tls/certs/ca-bundle.crt';
-  'smtp_tls_session_cache_database':  value   => 'btree:${data_directory}/smtp_tls_session_cache';
-}
-
-}
-
+ include profile::install_postfix
 }
